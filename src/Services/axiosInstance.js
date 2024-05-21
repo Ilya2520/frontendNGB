812bcafd10710bb8912b8c50f36c05630b7f701a
Ilya2520 <91724425+Ilya2520@users.noreply.github.com>
2024-05-21 21:30:43 +0300

    axios change

--- src/Services/axiosInstance.js
+++ src/Services/axiosInstance.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
 import { BASE_URL } from '../constants';
+import Cookies from 'js-cookie';
 
 const instance = axios.create({
+   baseURL: BASE_URL,
    withCredentials: true,
-   baseURL: BASE_URL
-});
-
-export default instance;
+ });
+ 
+ instance.interceptors.request.use((config) => {
+   const token = Cookies.get('BEARER');
+   if (token) {
+     config.headers.Authorization = `Bearer ${token}`;
+   }
+   return config;
+ }, (error) => {
+   return Promise.reject(error);
+ });
+ 
+ export default instance;
